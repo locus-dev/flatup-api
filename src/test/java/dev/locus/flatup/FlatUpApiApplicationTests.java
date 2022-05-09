c206e18d14a3e0ecaeaef718c0132757a7496086
SantosPereira <pedrohenriquelemam@gmail.com>
2022-05-09 20:38:01 -0300

    Projeto starter gerado no Spring Tools Suite

---
+++ src/test/java/dev/locus/flatup/FlatUpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.locus.flatup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatUpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
