fc473f4dc1b3d2550e3b26f3a885b0454eeced83
pedro-ss <pedross.tool@gmail.com>
2022-05-24 21:21:46 -0300

    alterando builder e service de pessoa

--- src/main/java/dev/locus/flatup/pessoa/builder/PessoaBuilder.java
+++ src/main/java/dev/locus/flatup/pessoa/builder/PessoaBuilder.java
@@ -4,32 +4,33 @@ import org.springframework.stereotype.Component;
 
 import dev.locus.flatup.pessoa.model.Pessoa;
 import dev.locus.flatup.pessoa.model.PessoaDto;
+import dev.locus.flatup.usuario.model.Usuario;
 
 @Component
 public class PessoaBuilder {
 
     public PessoaDto builderDto(Pessoa pessoa) {
         return PessoaDto.builder()
-        .cnpj(null)
-        .cpf(null)
-        .dataNascimento(null)
-        .idPessoa(null)
-        .nome(null)
-        .telefone(null)
-        .usuarioId(null)
-        .build();
+                .cnpj(pessoa.getCnpj())
+                .cpf(pessoa.getCpf())
+                .dataNascimento(pessoa.getDataNascimento())
+                .idPessoa(pessoa.getIdPessoa())
+                .nome(pessoa.getNome())
+                .telefone(pessoa.getTelefone())
+                .usuarioId(pessoa.getUsuario().getIdUsuario())
+                .build();
     }
 
-    public Pessoa builderModel(PessoaDto pessoaDto) {
+    public Pessoa builderModel(PessoaDto pessoaDto, Usuario usuario) {
         return Pessoa.builder()
-        .cnpj(pessoaDto.getCnpj())
-        .cpf(pessoaDto.getCpf())
-        .dataNascimento(pessoaDto.getDataNascimento())
-        .idPessoa(pessoaDto.getIdPessoa())
-        .nome(pessoaDto.getNome())
-        .telefone(pessoaDto.getTelefone())
-        //.usuario(pessoaDto.getUsuarioId()) setar usuário em service
-        .build();
+                .cnpj(pessoaDto.getCnpj())
+                .cpf(pessoaDto.getCpf())
+                .dataNascimento(pessoaDto.getDataNascimento())
+                .idPessoa(pessoaDto.getIdPessoa())
+                .nome(pessoaDto.getNome())
+                .telefone(pessoaDto.getTelefone())
+                .usuario(usuario)
+                .build();
     }
 
 }
--- src/main/java/dev/locus/flatup/pessoa/service/PessoaService.java
+++ src/main/java/dev/locus/flatup/pessoa/service/PessoaService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
+import javax.xml.crypto.KeySelector.Purpose;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,16 @@ import org.springframework.stereotype.Service;
 import dev.locus.flatup.pessoa.builder.PessoaBuilder;
 import dev.locus.flatup.pessoa.model.PessoaDto;
 import dev.locus.flatup.pessoa.repository.PessoaRepository;
+import dev.locus.flatup.usuario.repository.UsuarioRepository;
 
 @Service
 public class PessoaService {
 
   @Autowired
   private PessoaBuilder builder;
+
+  @Autowired
+  private UsuarioRepository usuarioRepository;
 
   @Autowired
   private PessoaRepository repository;
@@ -33,8 +38,8 @@ public class PessoaService {
 
   @Transactional
   public PessoaDto salvar(PessoaDto pessoaDto) {
-
-    var pessoa = builder.builderModel(pessoaDto);
+    var usuario = usuarioRepository.findById(pessoaDto.usuarioId).orElseThrow();
+    var pessoa = builder.builderModel(pessoaDto, usuario);
     var pessoaSalvo = builder.builderDto(repository.save(pessoa));
     return pessoaSalvo;
   }
@@ -46,8 +51,9 @@ public class PessoaService {
 
   @Transactional
   public PessoaDto alterar(Long idPessoa, PessoaDto pessoaDto) {
+    var usuario = usuarioRepository.findById(pessoaDto.usuarioId).orElseThrow();
     pessoaDto.setIdPessoa(idPessoa);
-    var pessoa = builder.builderModel(pessoaDto);
+    var pessoa = builder.builderModel(pessoaDto, usuario);
     return builder.builderDto(repository.save(pessoa));
   }
 
