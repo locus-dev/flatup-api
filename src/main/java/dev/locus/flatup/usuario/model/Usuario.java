1c1945328dcc19428e11efad1ee4d31226ee3ff2
pedro-ss <pedross.tool@gmail.com>
2022-06-30 00:30:46 -0300

    adicionado spring security

--- src/main/java/dev/locus/flatup/usuario/model/Usuario.java
+++ src/main/java/dev/locus/flatup/usuario/model/Usuario.java
@@ -1,14 +1,22 @@
 package dev.locus.flatup.usuario.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import dev.locus.flatup.pessoa.model.Pessoa;
 import lombok.AllArgsConstructor;
@@ -22,8 +30,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "USUARIO")
-public class Usuario {
+public class Usuario implements UserDetails {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID")
@@ -38,4 +48,48 @@ public class Usuario {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "PESSOA_ID", referencedColumnName = "ID")
 	private Pessoa pessoa;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<Perfil> perfils = new ArrayList<>();
+
+	@Override
+	public String getPassword() {
+		
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		
+		return true;
+	}
+
+	@Override
+	public List<Perfil> getAuthorities() {
+		return perfils;
+	}
 }
