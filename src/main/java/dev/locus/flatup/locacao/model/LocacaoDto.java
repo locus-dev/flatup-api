abc0a296854f3ded2cca50b45a18bb15e3ea93ac
vini <viniciusquaresma14@hotmail.com>
2022-08-22 22:44:11 -0300

    atualizacaoPDF

--- src/main/java/dev/locus/flatup/locacao/model/LocacaoDto.java
+++ src/main/java/dev/locus/flatup/locacao/model/LocacaoDto.java
@@ -17,7 +17,7 @@ public class LocacaoDto {
 
 	@JsonProperty(value = "locacao_id")
 	private Long idLocacao;
-	
+
 	@NotNull(message = "O usuário deve ser informado.")
 	@JsonProperty(value = "usuario_id")
 	private Long idUsuarioFK;
@@ -26,10 +26,10 @@ public class LocacaoDto {
 	@JsonProperty(value = "imovel_id")
 	private Long idImovelFK;
 
-	@NotNull(message = "O contto de locação não pode ser vazio.")
+	@NotNull(message = "O campo de locação não pode ser vazio.")
 	@JsonProperty(value = "contrato_locacao_id")
 	private Long idContratoLocacaoFK;
-	
+
 	@NotNull(message = "O status da locação não pode ser vazio.")
 	@JsonProperty(value = "status_locacao")
 	private String statusLocacao;
